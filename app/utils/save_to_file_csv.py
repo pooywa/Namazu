007d2833debb59a1b48425fe36a68739c0f408b0
Pooya Shourangiz <pooyashourangiz@gmail.com>
2026-07-28 21:11:18 +0330

    fix: resolve formatting issues in save_csv function

--- app/utils/save_to_file_csv.py
+++ app/utils/save_to_file_csv.py
@@ -1,22 +1,12 @@
-<<<<<<< HEAD
 from pathlib import Path
 
 import pandas
 
-def save_csv(filename:str,data:list[dict]):
+
+def save_csv(filename: str, data: list[dict]):
     output_file = Path(filename)
     output_file.parent.mkdir(parents=True, exist_ok=True)
 
     df = pandas.DataFrame(data)
 
-    df.to_csv(output_file,index=False,encoding='utf-8-sig')
-=======
-import pandas
-
-def save_csv(filename:str,data:list[dict]):
-
-    df = pandas.DataFrame(data)
-
-    df.to_csv(filename,index=False,encoding='utf-8-sig')
-
->>>>>>> origin/develop
+    df.to_csv(output_file, index=False, encoding="utf-8-sig")
