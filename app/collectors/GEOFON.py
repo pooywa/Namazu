f574ea218a23433188f1afa78b67229ffaf0e329
ali_balochi <alibalochi1910@gmail.com>
2026-07-28 13:30:25 +0330

    reafactoring: add sum title to output csv file and use fucntion to impliment modolarity

--- app/collectors/GEOFON.py
+++ app/collectors/GEOFON.py
@@ -2,7 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime , timedelta
-import csv
+from app.utils.save_to_file_csv import save_csv
 
 #Date range: last 30 days
 end_date = datetime.today().date()
@@ -18,47 +18,59 @@ nmax = "1000"
 #customizing our url with desired config
 url = (f"https://geofon.gfz.de/eqinfo/list.php?datemin={start_date}&datemax={end_date}&latmax={latmax}&lonmin={lonmin}&lonmax={lonmax}&latmin={latmin}&magmin=&fmt=html&nmax={nmax}")
 
-#get the information
-site = requests.get(url).text
+def cleaning(data:str):
+    return data.strip()
 
-#parsing html data
-soup = BeautifulSoup(site,"html.parser")
+def fetch_data(url):
+    #get site
+    site = requests.get(url).text
+    return site
 
-#creating csv file and specfying the column of the table
-csv_file = open("JAPAN_GEOFON.csv",'w')
-csv_witer = csv.writer(csv_file)
-csv_witer.writerow(["id","magnitude","depth","time","place","latitude","longitude"])
-
-#lst of all the cards 
-list_of_cards = soup.find('div',class_="container-fluid eqlist")
-
-#looping though the cards for acceccing to the data that we needed 
-for id,a in enumerate(list_of_cards.find_all('a')):
+def extract_data(a):
 
     #Skip non-earthquake links that don't contain magnitude information.
     try: 
-        magnitude = a.find('span',class_="magbox").text
-
+        magnitude = cleaning(a.find('span',class_="magbox").text)
     except AttributeError:
-        continue
+        return None,None,None,None,None,None
 
-    #Some earthquake cards use a nested span for depth information.
     #If it doesn't exist, fall back to the text node.
     try: 
         a.find('span',class_="pull-right").find('span')["title"]
-        depth = a.find('span',class_="pull-right").find('span').text.replace("*", "")
+        depth = cleaning(a.find('span',class_="pull-right").find('span').text.replace("*", ""))
 
     except KeyError:
-        depth = a.find('span',class_="pull-right").contents[0]
+        depth = cleaning(a.find('span',class_="pull-right").contents[0])
 
-    time = a.find_all('div',class_="col-xs-12")[1].contents[0]
-    place = a.strong.text
-    Epicenter = a.find('div',class_="col-xs-12")["title"]
-    latitude = Epicenter.split(",")[0]
-    longitude = Epicenter.split(",")[1]
+    time = cleaning(a.find_all('div',class_="col-xs-12")[1].contents[0])
+    place = cleaning(a.strong.text)
+    Epicenter = cleaning(a.find('div',class_="col-xs-12")["title"])
+    longitude = cleaning(Epicenter.split(",")[0])
+    latitude = cleaning(Epicenter.split(",")[1])
 
-    #here we saving all the data that we need
-    csv_witer.writerow([id,magnitude,depth,time,place,latitude,longitude])
+    return time,longitude,latitude,place,magnitude,depth
 
-#and never ever forget to close opening file
-csv_file.close()
+def pars_data(page):
+    soup = BeautifulSoup(page,"html.parser")
+
+    #storage dict for save csv file
+    data = []
+
+    #lst of all the cards 
+    list_of_cards = soup.find('div',class_="container-fluid eqlist")
+
+    #looping though the cards for acceccing to the data that we needed 
+    for a in list_of_cards.find_all('a'):
+        time,longitude,latitude,place,magnitude,depth = extract_data(a)
+        if not time:
+            continue
+        data.append({"DateTime":time,"Latitude":latitude,"Longitude":longitude,"Depth(km)":depth,"Magnitude":magnitude,"Region":place})
+
+    return data
+
+def main():
+    page = fetch_data(url)
+    data = pars_data(page)
+    save_csv("JAPAN_GEOFON.csv",data)
+
+main()
