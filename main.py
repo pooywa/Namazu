417c99cdf6acf1f7c0f54aaa0e7ecb7a6ae7db54
ali_balochi <alibalochi1910@gmail.com>
2026-07-31 01:18:38 +0330

    unpin col_main

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from app.collectors import main as col_main
 from app.database import main as db_main
 from app.analytics import main as alyt_main
 if __name__ == "__main__":
-    # col_main()
+    col_main()
     db_main()
     alyt_main()
