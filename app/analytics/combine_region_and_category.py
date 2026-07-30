6d67eff9e60c51b2a43b5cd0d5b5b3e1b59462c1
amirhosseineilat <amirhosseineilat@gmail.com>
2026-07-30 23:47:49 +0330

    grouping data and designing it in terminal with tabulate

--- app/analytics/combine_region_and_category.py
+++ app/analytics/combine_region_and_category.py
@@ -1,10 +1,43 @@
-from sqlalchemy import func
+from sqlalchemy import func , select
 from app.database.models import Earthquake
 from app.database.configuration import session
+from tabulate import tabulate
 
-def grouping():
+def grouping(session):
 
-    with session as s:
+    with session:
+        try:
+            stmt = select(Earthquake.month,
+                          Earthquake.category,
+                          Earthquake.region,
+                          func.count("*").label("earthquake_count"),
+                          func.avg(Earthquake.magnitude).label("avg_magnitude"),
+                          func.avg(Earthquake.depth).label("AVERAGE DEPTH"))\
+                    .group_by(Earthquake.month,
+                              Earthquake.category,
+                              Earthquake.region)\
+                    .order_by(Earthquake.month,
+                              Earthquake.region,
+                              Earthquake.category)
 
-        stmt = s.query(Earthquake.time,Earthquake.category,Earthquake.region)
+            result = session.execute(stmt).all()
+            return result
+        except Exception as e:
+            print(e)
+            return []
+
+def main():
+
+    # with tabulate we print our results in a pretty way
+    result = grouping(session)
+    print(tabulate(result,
+                   headers=['month',
+                            'category',
+                            'region',
+                            'earthquake_count',
+                            'avg_magnitude',
+                            'avg_depth'],
+                    tablefmt="grid"))
+
+        
 
