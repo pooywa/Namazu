02b6fb808e7a20e3801405211bc0d72a954e46dc
zizisoufi <z.soofi.n@gmail.com>
2026-07-30 17:33:45 +0330

    fix models

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase,Mapped,mapped_column
 from sqlalchemy import Integer, String, Column
-from sqlalchemy import Float
+
 
 class Base(DeclarativeBase):
     pass
@@ -13,8 +13,8 @@ class Earthquake(Base):
     time: Mapped[str] = mapped_column(nullable=True)
     latitude:Mapped[str] = mapped_column(nullable=True)
     longitude:Mapped[str] = mapped_column(nullable=True)
-    depth: Mapped[float] = mapped_column(Float, nullable=True)
-    magnitude: Mapped[float] = mapped_column(Float, nullable=True)
+    depth: Mapped[str] = mapped_column(str, nullable=True)
+    magnitude: Mapped[str] = mapped_column(str, nullable=True)
     place:Mapped[str] = mapped_column(nullable=True)
     source:Mapped[str] = mapped_column(nullable=True)
     month: Mapped[int] = mapped_column(Integer, nullable=True)
