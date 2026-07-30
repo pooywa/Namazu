1ae0228ed633cfb36216f9f8bdcf54dcc661228a
ALIasgharBalochi <125689935+ALIasgharBalochi@users.noreply.github.com>
2026-07-31 01:23:17 +0330

    Feature/convert sql data type (#58)

--- app/analytics/__init__.py
+++ app/analytics/__init__.py
@@ -1,5 +1,6 @@
 from app.analytics.handel_null_invalid import main as validate_main
-
+from app.analytics.convert_column_type import main as conv_main
 
 def main():
     validate_main()
+    conv_main()
