ca4dca1a28b78bb208cc3beda0d37af4b5d7e448
ali_balochi <alibalochi1910@gmail.com>
2026-07-30 19:56:18 +0330

    import validate format  to the main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from app.collectors import main as col_main
 from app.database import main as db_main
-
+from app.analytics import main as alyt_main
 if __name__ == "__main__":
     col_main()
     db_main()
+    alyt_main()
