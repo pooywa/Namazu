ad3ed0f23c80ead23a876d08aadb969fb00e36e0
ali_balochi <alibalochi1910@gmail.com>
2026-07-31 01:11:55 +0330

    convert type

---
+++ app/analytics/convert_column_type.py
@@ -0,0 +1,48 @@
+from sqlalchemy import Select,text
+from app.database.db_manager import managedb
+from app.database.models import Earthquake
+from app.database.configuration import session
+from app.database.preview_data import show_tabls_name_type
+
+def conver_to_float():
+
+    stmt = Select(Earthquake)
+    earthquakes:list[Earthquake] = managedb.read(stmt,"all")
+    if earthquakes:
+        for earthquake in earthquakes:
+            depth_val = str(float(earthquake.depth))
+            mag_val = str(float(earthquake.magnitude))
+
+            managedb.update(object=earthquake,attr="depth",new_value=depth_val)
+            managedb.update(object=earthquake,attr="magnitude",new_value=mag_val)
+        print("convert depth and magnitude to float successfuly")
+
+def convert_table_type():
+    mag_text = """ALTER TABLE earthquakes
+ALTER COLUMN magnitude TYPE DOUBLE PRECISION
+USING magnitude::DOUBLE PRECISION;"""    
+
+    depth_text = """ALTER TABLE earthquakes
+ALTER COLUMN depth TYPE DOUBLE PRECISION
+USING depth::DOUBLE PRECISION;"""
+
+    time_text = """ALTER TABLE earthquakes
+ALTER COLUMN time TYPE TIMESTAMP
+USING time::TIMESTAMP;"""
+
+    list_text = [mag_text,depth_text,time_text]
+
+    for txt in list_text:
+        session.execute(text(txt))
+        session.commit()
+
+    print("table type successfuly changed ")
+
+
+def main():
+    conver_to_float()
+    convert_table_type()
+
+if __name__ == "__main__":
+    main()
+    
