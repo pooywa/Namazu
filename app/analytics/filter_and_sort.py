ef669e3252cd866fbb73dc73e651860fa64b6e43
amirhosseineilat <amirhosseineilat@gmail.com>
2026-08-01 00:28:51 +0330

    fixing the problem for percenting the datas in column by changing some adjacment

--- app/analytics/filter_and_sort.py
+++ app/analytics/filter_and_sort.py
@@ -5,37 +5,61 @@ from tabulate import tabulate
 
 def ten_recent_earthquakes():
     
-    stmt =  select(Earthquake)\
+    stmt =  select(
+                Earthquake.id,
+                Earthquake.time,
+                Earthquake.latitude,
+                Earthquake.longitude,
+                Earthquake.depth,
+                Earthquake.magnitude,
+                Earthquake.place,
+                Earthquake.source,
+                Earthquake.month,
+                Earthquake.category,
+                Earthquake.region,
+                Earthquake.notes,)\
             .where(Earthquake.magnitude > 6)\
             .order_by(Earthquake.time.desc(),Earthquake.magnitude.desc())\
             .limit(10)
     
-    result = managedb.read(stmt,'all')
+    result = managedb.read(stmt,'all_row')
     return result
 
-# def depth_and_mag():
+def depth_and_mag():
     
-#         stmt =  select(Earthquake)\
-#                 .where(Earthquake.magnitude > 6,Earthquake.depth < 50)
+        stmt =  select(
+                        Earthquake.id,
+                        Earthquake.time,
+                        Earthquake.latitude,
+                        Earthquake.longitude,
+                        Earthquake.depth,
+                        Earthquake.magnitude,
+                        Earthquake.place,
+                        Earthquake.source,
+                        Earthquake.month,
+                        Earthquake.category,
+                        Earthquake.region,
+                        Earthquake.notes,)\
+                .where(Earthquake.magnitude > 6,Earthquake.depth < 50)
 
-#         result = managedb.read(stmt,'all')
-#         return result
+        result = managedb.read(stmt,'all_row')
+        return result
 
-# def Record_count_for_each_source():
+def Record_count_for_each_source():
       
-#       stmt =  select(Earthquake.source,func.count("*"))\
-#               .group_by(Earthquake.source)
+      stmt =  select(Earthquake.source,func.count("*"))\
+              .group_by(Earthquake.source)
       
-#       result = managedb.read(stmt,'all')
-#       return result
+      result = managedb.read(stmt,'all_row')
+      return result
 
-# def Average_magnitude_grouped_by():
+def Average_magnitude_grouped_by():
 
-#     stmt =  select(Earthquake.region,Earthquake.source,func.avg(Earthquake.magnitude).label("avg_magnitude"))\
-#             .group_by(Earthquake.region,Earthquake.source).order_by(func.avg(Earthquake.magnitude).desc())
+    stmt =  select(Earthquake.region,Earthquake.source,func.avg(Earthquake.magnitude).label("avg_magnitude"))\
+            .group_by(Earthquake.region,Earthquake.source).order_by(func.avg(Earthquake.magnitude).desc())
 
-#     result = managedb.read(stmt,'all')
-#     return result
+    result = managedb.read(stmt,'all_row')
+    return result
 
 def main():
 
@@ -43,18 +67,19 @@ def main():
         print("=== Ten Most Recent Strong Earthquakes ===") 
         print(tabulate(result1,headers=["id","time","latetude","longitude","depth","magnitude","place","source","month","category","rigen","nots"],tablefmt="grid"))
 
-#     result2 = depth_and_mag()
-#     print("=== Earthquakes With Magnitude > 6 and Depth < 50 km ===")
-#     print(result2)
-# #     print(tabulate(result2,headers=["id","time","latetude","longitude","depth","magnitude","place","source","month","category","rigen","nots"],tablefmt="grid"))
+        result2 = depth_and_mag()
+        print("=== Earthquakes With Magnitude > 6 and Depth < 50 km ===")
+        
+        print(tabulate(result2,headers=["id","time","latetude","longitude","depth","magnitude","place","source","month","category","rigen","nots"],tablefmt="grid"))
 
-#     result3 = Record_count_for_each_source()
-#     print("=== Record Count by Source ===")
-#     print(result3)
-# #     print(tabulate(result3,headers=["source","numbers of source"],tablefmt="grid"))
+        result3 = Record_count_for_each_source()
+        print("=== Record Count by Source ===")
+      
+        print(tabulate(result3,headers=["source","numbers of source"],tablefmt="grid"))
 
 
-#     result4 = Average_magnitude_grouped_by()
-#     print("=== Average Magnitude by Region and Source ===")
-#     print(result4)
-# #     print(tabulate(result4,headers=["region","source","avarage"],tablefmt="grid"))
+        result4 = Average_magnitude_grouped_by()
+        print("=== Average Magnitude by Region and Source ===")
+        
+        print(tabulate(result4,headers=["region","source","avarage"],tablefmt="grid"))
+
