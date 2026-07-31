0be283457de60e7003e1c55fbf830a7d90cf4e62
amirhosseineilat <amirhosseineilat@gmail.com>
2026-07-31 18:42:16 +0330

    add rigion finder

--- app/analytics/fill_data_to_earthquakes.py
+++ app/analytics/fill_data_to_earthquakes.py
@@ -2,6 +2,7 @@ from sqlalchemy import select,extract,update
 from app.database.configuration import session
 from app.database.db_manager import managedb
 from app.database.models import Earthquake
+import re
 
 def add_month_to_table():
 
@@ -26,7 +27,25 @@ def add_label_of_categury():
     
 
 def add_rigion_finder():
-    pass
+    stmt = select(Earthquake)
+
+    result = managedb.read(stmt,"all")
+
+    for place in result:
+
+        if "," in place.place:
+            first_part_of_place = place.place.strip().lower().split(",")[0]
+
+            if 'km' in first_part_of_place:
+                region = first_part_of_place.strip().split(" ")[-1]
+
+                managedb.update('region',region,object=place)
+
+            else:
+                managedb.update('region',first_part_of_place,object=place)
+            
+        else:
+            managedb.update('region',place.place,object=place)
 
 def main():
 
