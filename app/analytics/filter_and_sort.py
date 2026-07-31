0b527936503d2c002470d0f5486cdbd4411b096a
amirhosseineilat <amirhosseineilat@gmail.com>
2026-07-31 13:03:06 +0330

    Feature/filtering and source analysis (#60)

---
+++ app/analytics/filter_and_sort.py
@@ -0,0 +1,57 @@
+from sqlalchemy import func , select
+from app.database.models import Earthquake
+from app.database.db_manager import managedb
+from tabulate import tabulate
+
+def ten_recent_earthquakes():
+    
+    stmt =  select(Earthquake)\
+            .where(Earthquake.magnitude > 6)\
+            .order_by(Earthquake.time.desc(),Earthquake.magnitude.desc())\
+            .limit(10)
+    
+    result = managedb.read(stmt,'all')
+    return result
+
+def depth_and_mag():
+    
+        stmt =  select(Earthquake)\
+                .where(Earthquake.magnitude > 6,Earthquake.depth < 50)
+
+        result = managedb.read(stmt,'all')
+        return result
+
+def Record_count_for_each_source():
+      
+      stmt =  select(Earthquake.source,func.count("*"))\
+              .group_by(Earthquake.source)
+      
+      result = managedb.read(stmt,'all')
+      return result
+
+def Average_magnitude_grouped_by():
+
+    stmt =  select(Earthquake.region,Earthquake.source,func.avg(Earthquake.magnitude).label("avg_magnitude"))\
+            .group_by(Earthquake.region,Earthquake.source).order_by(func.avg(Earthquake.magnitude).desc())
+
+    result = managedb.read(stmt,'all')
+    return result
+
+def main():
+
+    result1 = ten_recent_earthquakes()
+    print("=== Ten Most Recent Strong Earthquakes ===")
+    print(tabulate(result1,tablefmt="grid"))
+    
+    result2 = depth_and_mag()
+    print("=== Earthquakes With Magnitude > 6 and Depth < 50 km ===")
+    print(tabulate(result2,tablefmt="grid"))
+
+    result3 = Record_count_for_each_source()
+    print("=== Record Count by Source ===")
+    print(tabulate(result3,tablefmt="grid"))
+
+
+    result4 = Average_magnitude_grouped_by()
+    print("=== Average Magnitude by Region and Source ===")
+    print(tabulate(result4,tablefmt="grid"))
