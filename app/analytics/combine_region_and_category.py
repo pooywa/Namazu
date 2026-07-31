4df054d3f7517323603ad7a95e1d93a2e86454ec
amirhosseineilat <amirhosseineilat@gmail.com>
2026-07-31 13:05:06 +0330

    Feature/combined region month and category (#61)

---
+++ app/analytics/combine_region_and_category.py
@@ -0,0 +1,38 @@
+from sqlalchemy import func , select
+from app.database.models import Earthquake
+from app.database.db_manager import managedb
+from tabulate import tabulate
+
+def grouping():
+
+            stmt = select(Earthquake.month,
+                          Earthquake.category,
+                          Earthquake.region,
+                          func.count("*").label("earthquake_count"),
+                          func.avg(Earthquake.magnitude).label("avg_magnitude"),
+                          func.avg(Earthquake.depth).label("avg_depth"))\
+                    .group_by(Earthquake.month,
+                              Earthquake.category,
+                              Earthquake.region)\
+                    .order_by(Earthquake.month,
+                              Earthquake.region,
+                              Earthquake.category)
+
+            result =  managedb.read(stmt,'all')
+
+
+def main():
+
+    # with tabulate we print our results in a pretty way
+    result = grouping()
+    print(tabulate(result,
+                   headers=['month',
+                            'category',
+                            'region',
+                            'earthquake_count',
+                            'avg_magnitude',
+                            'avg_depth'],
+                    tablefmt="grid"))
+
+        
+
