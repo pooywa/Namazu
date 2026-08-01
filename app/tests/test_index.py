60077a4c0bd39a08336222fbf6abea77b9d0d217
ali_balochi <alibalochi1910@gmail.com>
2026-08-01 23:00:55 +0330

    use postgresql instend of  sqlite

--- app/tests/test_index.py
+++ app/tests/test_index.py
@@ -1,8 +1,10 @@
 import unittest
 
-from sqlalchemy import text
+from sqlalchemy import text,create_engine
+from sqlalchemy.orm import Session
 from app.database.db_manager import managedb
-from app.database.create_indexes import main
+from app.database import configuration
+from app.analytics.add_index import main
 from app.database.models import Earthquake
 
 
@@ -10,11 +12,17 @@ class TestIndex(unittest.TestCase):
 
     
     def setUp(self):
-        engine = create_engine("sqlite:///:memory:")
-        test_session = Session(engine)
+        # engine = create_engine("sqlite:///:memory:")
+        # test_session = Session(engine)
+        # configuration.session = test_session
+        # managedb.session = test_session
+        # Earthquake.metadata.create_all(engine)
+        self.engine = create_engine("postgresql+psycopg://earthquakes_user:1234@localhost:5432/test_db_earthquakes")
+        test_session = Session(self.engine)
+        self.session = test_session
         configuration.session = test_session
         managedb.session = test_session
-        Earthquake.metadata.create_all(managedb.engine)
+        Earthquake.metadata.create_all(self.engine)
 
     def test_indexes_created(self):
 
@@ -42,3 +50,8 @@ class TestIndex(unittest.TestCase):
         self.assertIn("idx_earthquakes_region", indexes)
         self.assertIn("idx_earthquakes_time", indexes)
         self.assertIn("idx_earthquakes_magnitude", indexes)
+
+    def tearDown(self):
+        self.session.close()
+        Earthquake.metadata.drop_all(self.engine)
+        self.engine.dispose()
