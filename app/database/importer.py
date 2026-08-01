07f138e6d8704ba62a5107f940a092e3aad84236
amirhosseineilat <amirhosseineilat@gmail.com>
2026-08-01 16:50:41 +0330

    and vacumming def and add lan and lon fixer

--- app/database/importer.py
+++ app/database/importer.py
@@ -2,10 +2,63 @@ from pathlib import Path
 import pandas as pd
 from app.database.configuration import engine
 from app.database.mapping import COLLECTOR_MAPPING, MAPPINGS, detect_source
+import requests
+import time
 
 TABLE_NAME = "earthquakes"
 CHUNK_SIZE = 100
 
+def fix_lan_and_lon(out):
+        url = "https://nominatim.openstreetmap.org/search"
+    
+        headers = {
+            "User-Agent": "my-earthquake-project"
+        }
+    
+        df = out
+    
+        delete = []
+    
+        df["latitude"] = pd.to_numeric(df["latitude"], errors="coerce")
+        df["longitude"] = pd.to_numeric(df["longitude"], errors="coerce")
+    
+        df_result = (df["latitude"].isna() |
+                df["longitude"].isna())
+    
+        s = df[df_result]
+    
+        for index, row in s.iterrows():
+            place = row["place"]
+            params = {"q": place, "format": "json"}
+    
+            response = requests.get(url, params=params, headers=headers)
+    
+            if response.status_code != 200:
+                print(f"[{place}] request failed: {response.status_code}")
+                delete.append(index)
+                time.sleep(1)
+                continue
+    
+            r = response.json()
+    
+            if not r:
+                print(f"[{place}] no result found")
+                time.sleep(1)
+                continue
+    
+            df.loc[index, "latitude"] = float(r[0]["lat"])
+            df.loc[index, "longitude"] = float(r[0]["lon"])
+            print(f"[{place}] -> lat={r[0]['lat']}, lon={r[0]['lon']}")
+    
+            time.sleep(1)  
+    
+        df = df.drop(index=delete)
+
+def vacuuming(out):
+    
+    fix_lan_and_lon(out)
+
+    
 
 def import_one_file(file_path: Path) -> int:
     filename = file_path.name
@@ -19,7 +72,10 @@ def import_one_file(file_path: Path) -> int:
         out = chunk[required_cols].rename(columns=mapping).copy()
         out["source"] = source
 
-        out.to_sql(
+        #here we clean the data
+        result = vacuuming(out)
+
+        result.to_sql(
             name=TABLE_NAME,
             con=engine,
             if_exists="append",
