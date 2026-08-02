8ed5405db17d97d122324016c96bc471ca0df1ee
ALIasgharBalochi <125689935+ALIasgharBalochi@users.noreply.github.com>
2026-08-03 01:25:56 +0330

    Query/task 3 and 6 (#77)
    Co-authored-by: amirhosseineilat <amirhosseineilat@gmail.com>
    Co-authored-by: zizi <124803075+zizisoufi@users.noreply.github.com>

---
+++ app/query/query3.py
@@ -0,0 +1,31 @@
+from sqlalchemy import select , func
+from app.database.db_manager import managedb
+from app.database.models import Earthquake
+from tabulate import tabulate
+
+def rating_dangerous_earthquakes():
+
+    stmt = select(Earthquake.place,
+                  Earthquake.category,
+                  Earthquake.magnitude,
+                  Earthquake.depth)\
+                  .where(Earthquake.magnitude > 5 , Earthquake.depth < 50)\
+                  .order_by(Earthquake.magnitude.desc(),
+                            Earthquake.depth)\
+                  .limit(10)
+    
+    result = managedb.read(stmt,'all_row')   
+
+    return result
+
+def main():
+
+    print('\n === rating the most dangerous earthquakes ===\n ') 
+
+    dangerous_earthquacks = rating_dangerous_earthquakes()
+
+    print(tabulate(dangerous_earthquacks,
+                   ['place','category','average_magnitude','average_depth'],
+                   tablefmt="heavy_grid"))      
+    
+main()
---
+++ app/query/query6.py
@@ -0,0 +1,73 @@
+from sqlalchemy import select , func
+from app.database.db_manager import managedb
+from app.database.models import Earthquake
+from tabulate import tabulate
+
+def total_record_of_each_source():
+
+    stmt = select(Earthquake.source,func.count("*"))\
+           .group_by(Earthquake.source)
+
+    result = managedb.read(stmt,"all_row")
+    return result
+
+def avg_mag_record_of_each_source():
+
+    stmt = select(Earthquake.source,func.avg(Earthquake.magnitude))\
+           .group_by(Earthquake.source)
+
+    result = managedb.read(stmt,"all_row")
+    return result
+
+def avg_depth_record_of_each_source():
+
+    stmt = select(Earthquake.source,func.avg(Earthquake.depth))\
+           .group_by(Earthquake.source)
+
+    result = managedb.read(stmt,"all_row")
+    return result
+
+def number_of_record_by_area():
+    stmt = select(Earthquake.place,Earthquake.source,func.count("*"))\
+               .group_by(Earthquake.place,Earthquake.source)\
+               .order_by(Earthquake.place)
+    
+    result = managedb.read(stmt,"all_row")
+    return result
+
+def number_of_each_category():
+    stmt = select(Earthquake.source,Earthquake.category,func.count("*"))\
+               .group_by(Earthquake.source,Earthquake.category)\
+               .order_by(Earthquake.source)
+    
+    result = managedb.read(stmt,"all_row")
+    return result
+
+def main():
+
+    total = total_record_of_each_source()
+    print(tabulate(total,
+                    ['sources','number of sources'],
+                    tablefmt="heavy_grid"))
+
+    avg_mag = avg_mag_record_of_each_source()
+    print(tabulate(avg_mag,
+                    ['sources','average magnitude of each sources'],
+                    tablefmt="heavy_grid"))
+
+    avg_depth = avg_depth_record_of_each_source()
+    print(tabulate(avg_depth,
+                        ['sources','average depth of each sources'],
+                        tablefmt="heavy_grid"))
+
+    area = number_of_record_by_area()
+    print(tabulate(area,
+                    ['place','source',"number of area"],
+                    tablefmt="heavy_grid"))
+
+    category = number_of_each_category()
+    print(tabulate(category,
+                        ['source','category',"number of category"],
+                        tablefmt="heavy_grid"))
+
+main()
